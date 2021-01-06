4fa584631e627aea2478a4ffb55a50a798db1001
Vibin Varghese <vibinvvarghese@gmail.com>
2021-01-06 16:00:28 +0530

    First Commit

---
+++ mkdir.sh
@@ -0,0 +1,198 @@
+mkdir AS
+mkdir CC
+mkdir CD
+mkdir CJ
+mkdir CP
+mkdir CV
+mkdir DD
+mkdir DS
+mkdir GQ
+mkdir ID
+mkdir LP
+mkdir OL
+mkdir OR
+mkdir PJ
+mkdir RI
+mkdir SJ
+mkdir SK
+mkdir UL
+mkdir YD
+mkdir VM
+mkdir CD-RAMIS
+mkdir FS
+
+cd AS
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+
+cd ../CC
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../CDmkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../CJ
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../CP
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../CV
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../DD
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../DS
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../GQ
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../ID
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../LP
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../OL
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../OR
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../PJ
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../RI
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../SJ
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../SK
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../UL
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../YD
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../VM
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../CD-RAMIS
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
+
+cd ../FS
+mkdir batch-db2
+mkdir online-ims-imsdb
+mkdir online-cics-db2
+mkdir online-ims-db2
+mkdir batch-imsdb
+
