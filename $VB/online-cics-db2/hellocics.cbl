02b668518146b46bf786d4b48190a8d64deefd39
Vibin Varghese <vibinvvarghese@gmail.com>
2021-01-07 12:38:55 +0530

    hello world edits

--- $VB/online-cics-db2/hellocics.cbl
+++ $VB/online-cics-db2/hellocics.cbl
@@ -1,4 +1,4 @@
-       program-id. helloworld as "helloworld".
+       program-id. hellocics as "hellocics".
 
        environment division.
        configuration section.
@@ -8,10 +8,10 @@
         copy tstcopy.
        procedure division.
 
-           display "Hello There.".
+           display "Hello There CICS!".
            display "Hi There " MY-NAME.
            move "FEBIN" to MY-NAME.
            display "Hi There " MY-NAME.    
            goback.
 
-       end program helloworld.
+       end program hellocics.
